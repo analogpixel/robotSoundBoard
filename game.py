7c671873f7847a85530f859c682651e0939cda37
analog <matt.poepping@seagate.com>
2015-04-14 10:02:10 -0600

    added code for logic.

--- game.py
+++ game.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python
 
-
 import ConfigParser
 import os
+import sys
+import RPi.GPIO as GPIO
 
-
-# array that holds sound file names, and what pins they are attached to
+# array that holds sound file names, and what pins they are attached to (loaded from config)
 sounds = []
 # what sound is currently active
 activeSound = 0
+# what is going to play the sounds (loaded from config)
+aplay=""
+
+def flashPickNew(calcSound):
+  play(calcSound,0)
+
+def flashWrong(wrongSound):
+  play(wrongSound,0)
+
+def flashCorrect(correctSound):
+  play(correctSound,0)
+
+# wait is a bool 1 wait for sound to finish 0 don't wait
+def play(sound,wait):
+  system("%s wav/%s" % (aplay, sound)
 
 def setPins(pinConfig):
   for pin in pinConfig:
@@ -25,9 +40,23 @@ if __name__ == '__main__':
     for sound in config.options('sounds'):
       sounds.append( eval( config.get('sounds', sound)))
 
-    # if testing on a non pi, then set option
-    # to false
-    if config.getboolean('config', "useGPIO"):
-      import RPi.GPIO as GPIO
+    # configure each pin
+    setPins(sounds)
 
-      setPins(sounds)
+    aplay   = config.get('config', 'pathToAplay')
+    correct = config.get('config', 'correctSound')
+    wrong   = config.get('config', 'wrongSound')
+    calc    = config.get('config', 'calculatingSound')
+
+    # main game loop
+    while true:
+      play(sounds[activeSound])
+      i = getInput(aplay)
+      if i == activeSound:
+        flashCorrect(correct)
+        flashPickNew(calc)
+        activeSound = activeSound + 1
+        if activeSound > len(sounds):
+          activeSound = 0
+      else:
+        flashWrong(wrong)
