8c0b0699240198e6fdd52867f43fb235e0ac0a62
root <root@raspberrypi.(none)>
2015-04-16 03:04:44 +0000

    stuff

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ import os
 import sys
 import RPi.GPIO as GPIO
 import time
-
+import smbus
 
 # array that holds sound file names, and what pins they are attached to (loaded from config)
 sounds = []
@@ -14,8 +14,17 @@ activeSound = 0
 # what is going to play the sounds (loaded from config)
 aplay=""
 
+i2c=smbus.SMBus(1)
+addr=0x20
+ledshadow=0
+ledbit=[7, 6, 5, 4, 3, 2, 1, 0, 8, 9, 10, 11, 12, 13, 14, 15]
+
 def flashPickNew(calcSound):
   play(calcSound,0)
+  for i in range(15):
+    lamp_set(i, 1)
+    time.sleep(.1)
+    lamp_set(i,0)
 
 def flashWrong(wrongSound):
   play(wrongSound,0)
@@ -25,21 +34,37 @@ def flashCorrect(correctSound):
 
 # wait is a bool 1 wait for sound to finish 0 don't wait
 def play(sound,wait):
-  os.system("%s './wav/%s'" % (aplay, sound))
+  if wait:
+    os.system("%s './wav/%s'" % (aplay, sound))
+  else:
+    os.system("%s './wav/%s' &" % (aplay, sound))
 
-def pinSetup(pinConfig):
+def lamp_set(n, state):
+  global ledshadow
+  if (state) :
+    ledshadow |= 1<<(ledbit[n])
+    i2c.write_byte_data(addr, ledshadow & 0xff, ledshadow >> 8)
+  else :
+    ledshadow &= ~(1<<ledbit[n])
+    i2c.write_byte_data(addr, ledshadow & 0xff, ledshadow >> 8)
+  return
+
+def pinSetup(pinConfig, replay):
   GPIO.cleanup()
   GPIO.setmode(GPIO.BCM)
+
+  GPIO.setup( replay , GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
   for pin in pinConfig:
     GPIO.setup( pin['in'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
     #GPIO.add_event_detect( pin['in'], GPIO.FALLING) # , bouncetime=1000)
 
 # poll until you get something
-def getInput(pinConfig):
+def getInput(pinConfig, replay):
   while True:
     for pin in pinConfig:
-      #if GPIO.event_detected( int( pin['in'] ) ):
+      if not GPIO.input(replay):
+        return replay
       if not GPIO.input(int( pin['in'] ) ):
         return int(pin['id'])
 
@@ -52,13 +77,17 @@ if __name__ == '__main__':
     for sound in config.options('sounds'):
       sounds.append( eval( config.get('sounds', sound)))
 
-    # configure each pin
-    pinSetup(sounds)
 
-    aplay   = config.get('config', 'pathToAplay')
-    correct = config.get('config', 'correctSound')
-    wrong   = config.get('config', 'wrongSound')
-    calc    = config.get('config', 'calculatingSound')
+    aplay    = config.get('config', 'pathToAplay')
+    correct  = config.get('config', 'correctSound')
+    wrong    = config.get('config', 'wrongSound')
+    calc     = config.get('config', 'calculatingSound')
+    replayid = config.getint('config', 'replayid')
+
+    # configure each pin
+    pinSetup(sounds, replayid)
+
+    lamp_set(15,1)
 
     # main game loop
     while True:
@@ -67,8 +96,10 @@ if __name__ == '__main__':
         print sound
         while not match:
           play(sound['sound'],1)
-          i = getInput(sounds)
-          if i == sound['id']:
+          i = getInput(sounds,replayid)
+          if i == replayid:
+            continue
+          elif i == sound['id']:
             flashCorrect(correct)
             flashPickNew(calc)
             match=True
